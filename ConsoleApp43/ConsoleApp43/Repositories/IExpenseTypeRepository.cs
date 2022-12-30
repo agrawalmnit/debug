bb93293c9257cd1a2b02a986c95ef21e37eae51c
ishansi-abjima <119054363+ishansi-abjima@users.noreply.github.com>
2022-12-30 16:26:35 +0530

    Add files via upload

---
+++ ConsoleApp43/ConsoleApp43/Repositories/IExpenseTypeRepository.cs
@@ -0,0 +1,11 @@
+﻿//using ConsoleApp43;
+
+//namespace ConsoleApp43
+//{
+//    public interface IExpenseTypeRepository
+//    {
+//        IEnumerable<ExpenseTypeCategory> GetExpenseTypeCategories();
+//        IEnumerable<ExpenseType> GetExpenseTypes();
+//        IEnumerable<ExpenseType> GetExpenseTypesByCategoryId(int categoryId);
+//    }
+//}
